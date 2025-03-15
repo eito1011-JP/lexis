624e1bc8d05aa1e6ebcde08c3a882774d27e4b9a
諸橋瑛音 <eito.55855@gmail.com>
2025-03-15 11:44:14 +0900

    fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -77,7 +77,7 @@ const config: Config = {
         },
         // 管理画面へのリンクを追加
         {
-          href: 'https://app.pagescms.org/eito1011-jp/handbook/main/collection/Documents',
+          href: 'https://app.pagescms.org/eito1011-jp/handbook/cms-edits/collection/Documents',
           label: '管理画面',
           position: 'right',
         },
