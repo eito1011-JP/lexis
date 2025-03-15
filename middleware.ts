075a39d83b9cb7d02c11d89f56889e39aa06769d
諸橋瑛音 <eito.55855@gmail.com>
2025-03-15 16:29:57 +0900

    fix

--- middleware.ts
+++ middleware.ts
@@ -11,9 +11,10 @@ export default function middleware(request) {
     const basicAuth = authorizationHeader.split(" ")[1];
     const [user, password] = atob(basicAuth).toString().split(":");
 
-    if (user == process.env.BASIC_AUTH_USER && password == process.env.BASIC_AUTH_PASSWORD) {
-      return next();
-    }
+    // if (user == process.env.BASIC_AUTH_USER && password == process.env.BASIC_AUTH_PASSWORD) {
+    //   return next();
+    // }
+    return next();
   }
 
   return new Response("Basic Auth required", {
