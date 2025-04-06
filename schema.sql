b25dc75dff9ffd2f974222ee6f07f9b4adb97365
諸橋瑛音 <eito.55855@gmail.com>
2025-04-06 14:39:55 +0900

    add session table

--- schema.sql
+++ schema.sql
@@ -4,3 +4,12 @@ CREATE TABLE IF NOT EXISTS users (
   password TEXT NOT NULL,
   created_at TEXT DEFAULT (datetime('now'))
 );
+
+CREATE TABLE IF NOT EXISTS sessions (
+  id TEXT PRIMARY KEY,
+  user_id TEXT REFERENCES users(id) ON DELETE CASCADE,
+  sess TEXT NOT NULL,
+  expired DATETIME NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS sessions_expired_idx ON sessions (expired);
