d3050ab772cb0b67079a39310437e420830e905b
諸橋瑛音 <eito.55855@gmail.com>
2025-05-18 09:54:55 +0900

    fix

--- admin-frontend/src/pages/documents/[slug]/edit.tsx
+++ admin-frontend/src/pages/documents/[slug]/edit.tsx
@@ -247,15 +247,6 @@ export default function EditDocumentPage(): JSX.Element {
                 />
               </svg>
             </button>
-            <div className="ml-auto">
-              <button
-                className="px-4 py-2 bg-[#3832A5] text-white rounded hover:bg-opacity-80 border-none"
-                onClick={handleSave}
-                disabled={!!invalidSlug}
-              >
-                保存
-              </button>
-            </div>
           </div>
 
           <div className="mb-6">
@@ -357,6 +348,22 @@ export default function EditDocumentPage(): JSX.Element {
                 />
               </div>
             </div>
+          </div>
+
+          <div className="flex flex-col items-center gap-4 mt-8">
+            <button
+              className="px-4 py-2 bg-gray-700 text-white rounded hover:bg-opacity-80 border-none w-45"
+              onClick={() => window.open(`/documents/${documentSlug}`, '_blank')}
+            >
+              プレビュー
+            </button>
+            <button
+              className="px-4 py-2 bg-[#3832A5] text-white rounded hover:bg-opacity-80 border-none w-45"
+              onClick={handleSave}
+              disabled={!!invalidSlug}
+            >
+              保存
+            </button>
           </div>
         </div>
       </div>
