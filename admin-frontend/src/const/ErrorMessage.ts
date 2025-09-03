a45ec417d9936926c0abc41732ebed2df69f11de
eito1011-JP <eito.55855@gmail.com>
2025-09-04 07:57:39 +0900

    fix

---
+++ admin-frontend/src/const/ErrorMessage.ts
@@ -0,0 +1,20 @@
+export const ACCOUNT_LOCKED =
+  'アカウントがロックされました。暫く経ってから再度お試し下さい。';
+export const CONTINUOUS_FAILED = '連続してログインに失敗しています。';
+export const FAILED_USER_NAME_UPDATE =
+  'ユーザー名の登録でエラーが発生しました。';
+export const INCORRECT_EMAIL_PASSWORD =
+  'メールアドレス・パスワードが正しくありません。正しいメールアドレス・パスワードを入力してください。';
+export const INCORRECT_TWO_FA = '認証コードが正しくありません。';
+export const INTERNAL_ERROR = 'エラーが発生しました';
+export const INVALID_RESET_EMAIL_TOKEN =
+  '認証トークンが不正です、再度メールアドレス変更を実施してください。';
+export const INVALID_RESET_PASSWORD_TOKEN =
+  'リセットトークンが不正です、再度パスワード変更を実施してください。';
+export const RETRY_AFTER_ATTEMPT = '以降に再度お試しください。';
+export const STRING_LENGTH = '%s文字で入力してください。';
+export const MAX_STRING_LENGTH = '%s文字以内で入力してください。';
+export const MAX_FILE_LENGTH = '%sファイルまで添付できます。';
+export const DUPLICATE_EXECUTION = 'このユーザーはすでに登録されています';
+export const VALIDATION_ERROR = '入力内容に問題があります';
+export const ERROR = 'エラーが発生しました';
--- laravel-api/app/Http/Controllers/Api/EmailAuthnController.php
+++ laravel-api/app/Http/Controllers/Api/EmailAuthnController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Consts\ErrorType;
+use App\Exceptions\DuplicateExecutionException;
 use App\Http\Requests\Api\Auth\SendAuthnEmailRequest;
 use App\Http\Requests\Api\Auth\IdentifyTokenRequest;
 use App\Http\Requests\Api\Auth\SigninWithEmailRequest;
 use App\UseCases\Auth\SendAuthnEmailUseCase;
 use App\UseCases\Auth\IdentifyTokenUseCase;
 use App\UseCases\Auth\SigninWithEmailUseCase;
+use Exception;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
 
 class EmailAuthnController extends ApiBaseController
 {
@@ -23,7 +27,22 @@ class EmailAuthnController extends ApiBaseController
      */
     public function sendAuthnEmail(SendAuthnEmailRequest $request): JsonResponse
     {
-        $this->sendAuthnEmailUseCase->execute($request->email, $request->password);
+        try {
+            $this->sendAuthnEmailUseCase->execute($request->email, $request->password);
+        } catch (DuplicateExecutionException $e) {
+            return $this->sendError(
+                ErrorType::CODE_DUPLICATE_EXECUTION,
+                __('errors.MSG_DUPLICATE_EXECUTION'),
+                ErrorType::STATUS_DUPLICATE_EXECUTION,
+            );
+        } catch (Exception $e) {
+            Log::error($e);
+            return $this->sendError(
+                ErrorType::CODE_INTERNAL_ERROR,
+                __('errors.MSG_INTERNAL_ERROR'),
+                ErrorType::STATUS_INTERNAL_ERROR,
+            );
+        }
 
         return response()->json();
     }
