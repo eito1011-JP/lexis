5fba2a94cb0f590c68d83b7b539777d4f7445867
eito1011-JP <eito.55855@gmail.com>
2025-09-06 09:23:46 +0900

    fix not send duplicate mail

--- laravel-api/bootstrap/providers.php
+++ laravel-api/bootstrap/providers.php
@@ -3,7 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
-    App\Providers\EventServiceProvider::class,
     App\Providers\RepositoryServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
 ];
