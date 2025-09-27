1a2a39eed07690eb200d682d3e48b646cbc26e4b
eito1011-JP <eito.55855@gmail.com>
2025-09-27 18:19:05 +0900

    fix detail

--- laravel-api/app/UseCases/DocumentCategory/GetCategoryUseCase.php
+++ laravel-api/app/UseCases/DocumentCategory/GetCategoryUseCase.php
@@ -4,6 +4,7 @@ namespace App\UseCases\DocumentCategory;
 
 use App\Dto\UseCase\DocumentCategory\GetCategoryDto;
 use App\Models\DocumentCategory;
+use App\Models\DocumentCategoryEntity;
 use Http\Discovery\Exception\NotFoundException;
 
 /**
@@ -18,8 +19,11 @@ class GetCategoryUseCase
      */
     public function execute(GetCategoryDto $dto): array
     {
+        $categoryEntity = DocumentCategoryEntity::find($dto->categoryEntityId);
+
+        // entityに紐づくdocumentCategoryの
         $category = DocumentCategory::with(['parent.parent.parent.parent.parent.parent.parent']) // 7階層まで親カテゴリを読み込み
-            ->where('id', $dto->id)
+            ->where('id', $categoryEntity->id)
             ->where('organization_id', $dto->user->organizationMember->organization_id)
             ->first();
 
